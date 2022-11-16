5257fe82ee0f621d1f08813ea93ed122461ff758
Artem <mntp112@yandex.ru>
2022-11-16 13:31:20 +0300

    fix: array content card discription fix

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ const initialCards = [
     link: 'https://images.unsplash.com/photo-1605907153179-8b364644a241?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1630&q=80'
   },
   {
-    name: 'Nissan Sylvia S13',
+    name: 'Nissan Silvia S13',
     link: 'https://images.unsplash.com/photo-1595000728060-8555b134374e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
   },
   {
